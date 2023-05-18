34cb7c70bf3405525ec8a4a25ad09e125b51598d
Abdullah149081 <masudmd425@gmail.com>
2023-05-19 01:40:08 +0600

    navbar done

--- src/page/shared/Navbar/Navbar.jsx
+++ src/page/shared/Navbar/Navbar.jsx
@@ -1,9 +1,51 @@
-
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useState } from "react";
+import { FaStream } from "react-icons/fa";
+import { TiTimesOutline } from "react-icons/ti";
+import { Link } from "react-router-dom";
+import nav from "../../../assets/nav.png";
+import ActiveLink from "./ActiveLink";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>
-      <h1>hi</h1>
+    <div className="md:sticky top-0 z-10">
+      <nav className="lg:px-32 relative bg-[#9363b5] py-4 flex  justify-between items-center pl-4 ">
+        <img className="hidden lg:block absolute top-[70px] z-0 left-0 w-full" src={nav} alt="" />
+        <div>
+          <Link to="/">
+            <h2 className="text-4xl font-bold text-[#ffff]">Toy House</h2>
+          </Link>
+        </div>
+
+        <div>
+          <div
+            onClick={() => {
+              setOpen(!open);
+            }}
+            role="button"
+            tabIndex="0"
+          >
+            <span className="pr-12 md:pr-0 ">
+              {open ? <TiTimesOutline className="h-6 w-7 mt-7   md:hidden text-gray-900 cursor-pointer" /> : <FaStream className="  h-5 w-5 mt-7 md:hidden text-gray-900  cursor-pointer" />}
+            </span>
+          </div>
+
+          <div
+            className={`flex flex-col md:py-0 md:space-x-10  divide-neutral-300  divide-y lg:divide-y-0  lg:w-full font-bold text-[#ffff]  md:static md:flex-row  md:block ${
+              open ? "top-22 left-0  w-1/2 text-left bg-[#9363B5] py-12  px-6 space-y-6  rounded-br-lg absolute" : "hidden"
+            }`}
+          >
+            <ActiveLink to="/">Home</ActiveLink>
+            <ActiveLink to="/all-toys">All Toys</ActiveLink>
+            <ActiveLink to="/blog">Blog</ActiveLink>
+            <ActiveLink to="/add-toy">Add A Toy</ActiveLink>
+            <ActiveLink to="/my-toys">My Toys</ActiveLink>
+            <ActiveLink to="/sing-in">Sing in</ActiveLink>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
