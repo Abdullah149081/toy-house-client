7979b6a1ca7f8f1d45396772ca87fa54c198b8ac
Abdullah149081 <masudmd425@gmail.com>
2023-05-19 22:40:10 +0600

    spinner  add

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,21 @@
+import { useContext } from "react";
 import { Outlet } from "react-router-dom";
+import GridLoader from "react-spinners/GridLoader";
 import Navbar from "../page/shared/Navbar/Navbar";
+import { AuthContext } from "../providers/AuthProviders";
 
 const Main = () => {
+  const { loading } = useContext(AuthContext);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex flex-col gap-8 justify-center items-center">
+        <h2 className="text-4xl font-bold animate-ping">Toy House</h2>
+        <GridLoader color="#ff8c9a" />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Navbar />
