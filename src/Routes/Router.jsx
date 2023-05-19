be36d281bb0dd6caab6307c01326be82f26ce6d0
Abdullah149081 <masudmd425@gmail.com>
2023-05-19 22:47:03 +0600

    Error page done

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../page/Home/Home/Home";
+import ErrorPage from "../page/shared/ErrorPage/ErrorPage";
 import Login from "../page/shared/Login/Login/Login";
 import Register from "../page/shared/Login/Register/Register";
 
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
---
+++ src/page/shared/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,24 @@
+import { Link, useRouteError } from "react-router-dom";
+import "./ErrorPage.css";
+
+export default function ErrorPage() {
+  const { error } = useRouteError();
+
+  return (
+    <div id="notfound">
+      <div className="notfound px-4 pt-40 w-full max-w-xl lg:pt-0 lg:pr-0 lg:pl-40">
+        <div className="notfound-404 " />
+        <div className="space-y-4">
+          <h1 className="text-7xl font-bold">404</h1>
+          <h2 className="text-2xl font-bold uppercase">Oops! Page Not Be Found</h2>
+          <p className="font-medium text-lg text-error">{error?.status || error?.message}</p>
+          <Link className="inline-block" to="/">
+            <button type="button" className="btn btn-toy">
+              Back to homepage
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
