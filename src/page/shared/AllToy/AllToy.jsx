209b807f81fc54a67af6a1061abeef4cbd84a87b
Abdullah149081 <masudmd425@gmail.com>
2023-05-21 17:33:42 +0600

    all toys page done

--- src/page/shared/AllToy/AllToy.jsx
+++ src/page/shared/AllToy/AllToy.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
 import PageTitle from "../../../PageTitle/PageTitle";
